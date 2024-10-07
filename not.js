7024a43d56c8895a1dcae4680bfcf4fcfb0c09e4
RicardoMatos <ricardoribeirodematos@gmail.com>
2024-10-07 19:56:12 -0300

    andamento

--- not.js
+++ not.js
@@ -4,5 +4,13 @@ function alterarTema(){
     if(body.classList.contains('dark-theme')){
         body.classList.remove('dark-theme'); //remove o tema
         body.classList.add('light-theme'); //Adiciona o tema
+    } else {
+        body.classList.remove('light-theme')
+        body.classList.add('dark-theme')
     }
+}
+
+function mudarCorTitulo(){
+    var titulo = document.getElementById('logo');
+    titulo.style.color = core
 }
